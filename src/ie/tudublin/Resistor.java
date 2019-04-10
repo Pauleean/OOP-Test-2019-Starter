55375d00e543f3a66a22e25e6eea26da7f6fa920
Niall <C17511003@mydit.ie>
2019-04-10 16:49:01 +0100

    Added Resistor class

---
+++ src/ie/tudublin/Resistor.java
@@ -0,0 +1,17 @@
+package ie.tudublin;
+
+public class Resistor
+{
+    public int value;
+    public int ones;
+    public int tens;
+    public int hundreds;
+
+    public Resistor(int value)
+    {
+        this.value = value;
+        hundreds = (value / 100);
+        tens = (value - (hundreds * 100)) / 10;
+        ones = value - ((hundreds * 100)  + (tens * 10));
+    }
+}
